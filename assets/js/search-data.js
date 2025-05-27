8c775cfdf081430244efab8b7c29912c6cb60dd5
jkudlerflam <jkudlerflam@gmail.com>
2025-05-27 02:32:57 +0000

    Deploying to gh-pages from @ jkudlerflam/jkudlerflam.github.io@ddffd306b3873c054fd79b221a13b2b67509f583 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -487,14 +487,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%6B%75%64%6C%65%72%66%6C%61%6D@%69%61%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/1626495", "_blank");
         },
       },{
         id: 'social-rss',
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=Nj124O8AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
