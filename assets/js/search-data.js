538da2a1bae2815968bedae7d568375b7c9c746e
jkudlerflam <jkudlerflam@gmail.com>
2026-08-16 18:57:17 +0000

    Deploying to gh-pages from @ jkudlerflam/jkudlerflam.github.io@6279c44ee67bb59a191c1896362d25dd4a69598c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "CV Updated 5/16/26",
+          description: "CV Updated 8/14/26",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -110,7 +110,7 @@ ninja.data = [{
               window.location.href = "/";
             },},{id: "pages-cv",
           title: 'cv',
-          description: "CV Updated 5/16/26",
+          description: "CV Updated 8/14/26",
           section: "Pages",handler: () => {
               window.location.href = "/cv/";
             },},{id: "pages-news",
